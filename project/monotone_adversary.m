56809640c03d39928b489543511a90877969d62f
ChristophorusX <mrxrtxrt@gmail.com>
2017-08-06 19:56:30 -0400

    update project

--- project/monotone_adversary.m
+++ project/monotone_adversary.m
@@ -1,22 +1,57 @@
 % This is a MATLAB program of running monotone adversary on community
 % detection problem of 2 clusters. @author: Ruitu Xu
 
-% Generate random adjacency matrix of dimension n (even) according to inner and
-% inter cluster edge probability p and q
-number_of_vertices = 1000;
+% Generate random adjacency matrix (symmetric) of dimension n (even) 
+% according to inner- and inter-cluster edge probability p and q
+number_of_vertices = 300;
 a = 4;
 b = 6;
 n = number_of_vertices;
-p = a * log(n) / n;
-q = b * log(n) / n;
-R1 = rand(n/2,n/2) > (1-p);
+p = a*log(n)/n;
+q = b*log(n)/n;
+R1_pre = rand(n/2,n/2) > (1-p);
+%display(R1_pre)
+R3_pre = rand(n/2,n/2) > (1-p);
+%display(R3_pre)
+for i = 1:(n/2)
+    for j = 1:i
+        if j < i
+            R1_pre(i,j) = R1_pre(j,i);
+            R3_pre(i,j) = R3_pre(j,i);
+        else
+            R1_pre(i,i) = 1;
+            R3_pre(i,i) = 1;
+        end
+    end
+end
+%display(R1_pre)
+%display(R3_pre)
+
 R2 = rand(n/2,n/2) > (1-q);
-A = [R1 R2; R2 R1];
+A = [R1_pre R2; R2' R3_pre];
+%display(A)
 row_sum = sum(A,2);
 for i = 1:n
-    disp(['The summation of row ' num2str(i) ' is ' num2str(row_sum(i)) '.'])
+    disp(['The summation of row #' num2str(i) ' is ' num2str(row_sum(i)) '.'])
 end
 disp(['The average edge of every row should be around ' num2str((p*n + q*n)/2) '.'])
 disp(['The empirical average number of edges is ' num2str(sum(row_sum)/n)])
-    
+
+% Perform a manifold optimization on the cost function Tr(AX) given the
+% manifold (Symmetric positive semidefinite, fixed-rank with unit diagonal)
+% with rank(X)<=2
+
+k = 2;
+manifold = elliptopefactory(n, k);
+problem.M = manifold;
+
+problem.cost = @(X) -sum(diag((A*X)));
+
+[X, Xcost, info] = trustregions(problem);
+
+figure;
+semilogy([info.iter], [info.gradnorm], '.-');
+xlabel('Iteration #');
+ylabel('Gradient norm');
+title('Convergence of the trust-regions algorithm on the manifold');
 
