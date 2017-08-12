fa68e3c112c714a1225ef0f1b218b66be114e313
ChristophorusX <mrxrtxrt@gmail.com>
2017-08-11 21:49:16 -0400

    trying

--- project/break_spectral_on_partial_recovery.m
+++ project/break_spectral_on_partial_recovery.m
@@ -25,24 +25,24 @@ for iteration = 10:num_of_trails
         disp(['Working on repitition ' num2str(repitition) '.'])
         [ A, z_sbm ] = generate_sbm_adjacency(n, a, b);
         % Put matrix A into monotone adversary.
-        V = monotone_adversary( A, a, b );
+        % V = monotone_adversary( A, a, b );
         % display(sum(tagged_vector))
         % display(sum(sum(A-V)))
         % display(good_vector)
         [ clustering_A, eigenvalues_A, eigenvectors_A ] = spectral_clustering_rw( A, 2 );
-        [ clustering_V, eigenvalues_V, eigenvectors_V ] = spectral_clustering_rw( V, 2 );
+        % [ clustering_V, eigenvalues_V, eigenvectors_V ] = spectral_clustering_rw( V, 2 );
         error_rate_A = compute_error_rate(clustering_A,z_sbm);
-        error_rate_V = compute_error_rate(clustering_V,z_sbm);
+        % error_rate_V = compute_error_rate(clustering_V,z_sbm);
         disp(['Error rate for recovery from A is ' num2str(error_rate_A)])
-        disp(['Error rate for recovery from V is ' num2str(error_rate_V)])
-        % if error_rate_A < (1 - percentage)
-        %     disp(['GOOD RESULT on random model with parameter a = ' num2str(a) ', b = ' ...
-        %     num2str(b) ', and over threshold with amount ' num2str(jump)]);
-        % else
-        %     disp(['BAD RESULT on random model with parameter a = ' num2str(a) ', b = ' ...
-        %     num2str(b) ', and over threshold with amount ' num2str(jump)]);
-        %     fail_records_A(iteration) = fail_records_A(iteration) + 1;
-        % end
+        % disp(['Error rate for recovery from V is ' num2str(error_rate_V)])
+        if error_rate_A < (1 - percentage)
+            disp(['GOOD RESULT on random model with parameter a = ' num2str(a) ', b = ' ...
+            num2str(b) ', and over threshold with amount ' num2str(jump)]);
+        else
+            disp(['BAD RESULT on random model with parameter a = ' num2str(a) ', b = ' ...
+            num2str(b) ', and over threshold with amount ' num2str(jump)]);
+            fail_records_A(iteration) = fail_records_A(iteration) + 1;
+        end
         % if error_rate_V < (1 - percentage)
         %     disp(['GOOD RESULT on semi-random model with parameter a = ' num2str(a) ', b = ' ...
         %     num2str(b) ', and over threshold with amount ' num2str(jump)]);
