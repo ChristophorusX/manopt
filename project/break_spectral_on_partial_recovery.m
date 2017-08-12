8c5dc4c58af03f4d52f663f5b09a6a674be82026
ChristophorusX <mrxrtxrt@gmail.com>
2017-08-12 11:45:51 -0400

    i

--- project/break_spectral_on_partial_recovery.m
+++ project/break_spectral_on_partial_recovery.m
@@ -9,7 +9,7 @@ n = 5000;
 % Fix a percentage of partial recovery.
 percentage = 0.5;
 % Set a to a fixed number and the set b according to the treshold.
-a = 25;
+a = 5;
 % Set an array recording the times of failing trails.
 num_of_trails = 15;
 num_of_repititions = 100;
@@ -19,7 +19,8 @@ for iteration = 14:num_of_trails
     disp(['Working on iteration ' num2str(iteration) '.'])
     jump = iteration/1;
     % obtain b from a and threshold.
-    b = ((2*a+(2+jump))-sqrt((2*a+(2+jump))^2-4*a*(a-(2+jump))))/2;
+    % b = ((2*a+(2+jump))-sqrt((2*a+(2+jump))^2-4*a*(a-(2+jump))))/2;
+    b = 2;
     disp(['b = ' num2str(b)])
     % A_store = zeros(n,n,num_of_repititions);
     % V_store = zeros(n,n,num_of_repititions);
