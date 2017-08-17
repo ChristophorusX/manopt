8dc7362a4459886c9de7ddea69fa39ff7fd2f42d
ChristophorusX <mrxrtxrt@gmail.com>
2017-08-17 12:16:32 -0400

    iiiii

--- project/k_means_rows.m
+++ project/k_means_rows.m
@@ -1,4 +1,4 @@
 function [ clustering ] = k_means_rows( Q )
-    clustering_pre = kmeans(Q, 2, 'MaxIter', 10000000);
+    clustering_pre = kmeans(Q, 2, 'MaxIter', 1000000000);
     clustering = 2 * (clustering_pre - 1) - 1;
 end
