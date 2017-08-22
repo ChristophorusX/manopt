f6786f095b1400decbff86c141bbb60327f2c43a
ChristophorusX <mrxrtxrt@gmail.com>
2017-08-22 10:38:36 -0400

    ii

--- project/break_BM.m
+++ project/break_BM.m
@@ -3,8 +3,8 @@ warning('off', 'manopt:elliptopefactory:exp')
 n = 1000;
 b = 5;
 num_of_trails = 10;
-num_of_repititions = 5;
-density_of_jump = 5;
+num_of_repititions = 3;
+density_of_jump = 1;
 lambda_base = 10;
 delta = 1/10;
 percent_of_elements_being_one = 0.5;
