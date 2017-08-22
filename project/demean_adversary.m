e44ff19654afd22904bb5086273a870c4ddadcae
ChristophorusX <mrxrtxrt@gmail.com>
2017-08-22 10:42:10 -0400

    fix bug

--- project/demean_adversary.m
+++ project/demean_adversary.m
@@ -6,6 +6,6 @@ function [ V_bm ] = demean_adversary( V )
 [ dimension, ~ ] = size(V);
 sum_col = sum(V, 2);
 one_vector = ones(dimension, 1);
-V_bm = V - sum_col * one_vector';
+V_bm = V - sum_col * one_vector' / dimension;
 
 end  % function
