ef39d3ffa839eac45bcd4aebe225cd486c6d49d3
ChristophorusX <mrxrtxrt@gmail.com>
2017-10-17 11:22:32 -0400

    add spectral gap test.

---
+++ project/compare_spectral_gap_against_adversaries.m
@@ -0,0 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is a program verifying the spectral gap increase of advhronization
+% problem under rank 2 Burer-Monteiro approach against monotone adversary.
+%
+% @author: Ruitu Xu
+% NOTE: this setting is for advhronization problem.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Generating the observed advhronization matrix.
+Y_adv = generate_synchronization_gaussian(100, 0.5, 10);
+% Generating the advhronization matrix under monotone adversary.
+Y_adv = monotone_adversary_adv(Y_sync, 1);
+% Perform BM on both matrix.
+[ Q_Y_adv, ~, ~, ~ ] = burer_monteiro( Y_sync );
+[ Q_Y_adv, ~, ~, ~ ] = burer_monteiro( Y_adv );
+% Construct S
+S_sync = diag(diag(Y_sync * Q_Y_sync * Q_Y_sync')) - Y_sync;
+S_adv = diag(diag(Y_adv * Q_Y_adv * Q_Y_adv')) - Y_adv;
+% Compute eigenvalues of S
+e_sync = eig(S_sync);
+e_adv = eig(S_adv);
+% Sorting eigenvalues
+eigenvalues_sync = sort(e_sync);
+eigenvalues_adv = sort(e_adv);
+% Compute spectral gap increase.
+spectral_gap_increase = eigenvalues_adv(2) - eigenvalues_sync(2);
+disp(['The spectral gap increase is ' num2str(spectral_gap_increase)]);
---
+++ project/monotone_adversary_sync.m
@@ -0,0 +1,22 @@
+function [ Y_adv ] = monotone_adversary_sync( Y_sync, sigma )
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Generate random matrix Y_adv of dimension n for Z2 synchronization
+% problem by modifying the observed matrix with a Gaussian monotone
+% noise.
+% @author: Ruitu Xu
+%
+% @parameter: Observed synchronization matrix Y_sync.
+% @parameter: The standard deviation sigma of the monotone noise.
+%
+% @return: The observed random matrix Y_sync (with monotone noise).
+% NOTE: this setting is for synchronization problem.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rng('shuffle');
+[n,~] = size(Y_sync);
+M = normrnd(0,sigma,n,n);
+M_plus = abs(M);
+M_pre = M_plus .* (Y_sync != 0);
+multiplier = (Y_sync > 0) * 2 - ones(n,n);
+Y_adv = Y_sync + multiplier .* M_pre;
+disp('Monotone adversary matrix of Z2 synchronization is generated!')
+end
