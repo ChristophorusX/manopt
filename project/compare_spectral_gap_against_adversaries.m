1e92f76a19dcbf7709b259a299bcc36af475cadb
ChristophorusX <mrxrtxrt@gmail.com>
2017-10-17 13:45:02 -0400

    debugging

--- project/compare_spectral_gap_against_adversaries.m
+++ project/compare_spectral_gap_against_adversaries.m
@@ -8,20 +8,22 @@
 warning('off', 'manopt:getHessian:approx')
 % Generating the observed advhronization matrix.
 % Generating the advhronization matrix under monotone adversary.
-[ Y_sync, Y_adv] = monotone_adversary_sync(100, 0.5, 10, 1)
+[ Y_sync, Y_adv ] = monotone_adversary_sync(100, 0.5, 10, 1);
 % disp(((Y_adv - Y_sync) .* Y_sync) > 0);
 % Perform BM on both matrix.
 [ Q_Y_sync, ~, ~, ~ ] = burer_monteiro( Y_sync );
 [ Q_Y_adv, ~, ~, ~ ] = burer_monteiro( Y_adv );
 % Construct S
-S_sync = diag(diag(Y_sync * Q_Y_sync * Q_Y_sync')) - Y_sync;
-S_adv = diag(diag(Y_adv * Q_Y_adv * Q_Y_adv')) - Y_adv;
+S_sync = diag(diag(Y_sync * (Q_Y_sync * Q_Y_sync'))) - Y_sync;
+S_adv = diag(diag(Y_adv * (Q_Y_adv * Q_Y_adv'))) - Y_adv;
 % Compute eigenvalues of S
 e_sync = eig(S_sync);
 e_adv = eig(S_adv);
 % Sorting eigenvalues
 eigenvalues_sync = sort(e_sync);
 eigenvalues_adv = sort(e_adv);
+disp(['spectrum of the sync is: ' num2str(eigenvalues_sync(1)) ',' num2str(eigenvalues_sync(2)) ',' num2str(eigenvalues_sync(3))]);
+disp(['spectrum of the adv is: ' num2str(eigenvalues_adv(1)) ',' num2str(eigenvalues_adv(2)) ',' num2str(eigenvalues_adv(3))]);
 % Compute spectral gap increase.
 spectral_gap_increase = eigenvalues_adv(2) - eigenvalues_sync(2);
 disp(['The spectral gap increase is ' num2str(spectral_gap_increase)]);
